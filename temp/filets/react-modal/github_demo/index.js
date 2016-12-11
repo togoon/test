0b9a8ba28169fb2ee7b14665d8ace9e5d4caa8e6
insanehui <269200808@qq.com>
2016-12-11 11:15:33 +0800

    add react-modal demo

---
+++ temp/filets/react-modal/github_demo/index.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Modal from 'react-modal';
+
+
+/*
+The app element allows you to specify the portion of your app that should be hidden (via aria-hidden)
+to prevent assistive technologies such as screenreaders from reading content outside of the content of
+your modal.  It can be specified in the following ways:
+
+* element
+Modal.setAppElement(appElement);
+
+* query selector - uses the first element found if you pass in a class.
+Modal.setAppElement('#your-app-element');
+
+*/
+var appElement = document.getElementById('root');
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+
+var App = React.createClass({
+
+  getInitialState: function() {
+    return { modalIsOpen: false };
+  },
+
+  openModal: function() {
+    this.setState({modalIsOpen: true});
+  },
+
+  afterOpenModal: function() {
+    // references are now sync'd and can be accessed.
+    this.refs.subtitle.style.color = '#f00';
+  },
+
+  closeModal: function() {
+    this.setState({modalIsOpen: false});
+  },
+
+  render: function() {
+    return (
+      <div>
+        <button onClick={this.openModal}>Open Modal</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref="subtitle">Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <div>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+});
+
+render(<App/>, appElement);

69089b61ac9e9c0619faa6f63befb1ce36cacbb2
insanehui <269200808@qq.com>
2016-12-16 16:20:49 +0800

    准备增加一个组件库标题

--- temp/filets/draft/ToolPanel.js
+++ temp/filets/draft/ToolPanel.js
@@ -1,17 +1,31 @@
 import React, { PureComponent as Component, PropTypes } from 'react';
-import Radium from 'radium';
-import { border, ptr, bg, hsl, flex, sz } from './utils/cssobj.js'
+import { css, border, ptr, bg, hsl, flex, sz } from './utils/cssobj.js'
+import cx from 'classnames'
 
-const S = {
+const S = css({
+
   main: {
     ...border, ...flex,
     width: 150,
+    flexWrap: "wrap",
+    alignContent: "flex-start",
   },
 
   item: {
-    ...border, ...ptr, ...sz(70),
-  }
-}
+    ...flex, ...border, ...ptr, ...sz(70),
+    // TODO: 后面用 >< 标记来表示这两个center，可以选的符号有：< > ^ v <|> >|< << >> <^ <-|> 后续好好研究一下它们的意义
+    justifyContent: "center", 
+    alignItems: "center",
+  },
+
+  picked: bg(hsl(178, 32, 75)),
+
+  title : {
+    
+  },
+  
+
+})
 
 class ToolPanel extends Component {
 
@@ -23,6 +37,7 @@ class ToolPanel extends Component {
       data : [
         "Mysql",
         "Storage",
+        "Tomcat",
       ],
       sel : p.sel, // 当前选中的，缺省为null
     }
@@ -41,9 +56,10 @@ class ToolPanel extends Component {
 
   render() {
     let s = this.state
-    return <div style={S.main}>
+    return <div className={S.main}>
+      <div className={S.title}>组件库</div>
       {s.data.map( (v,i) => 
-        <div key={i} style={[S.item, i === s.sel ? bg(hsl(178, 32, 75)) : null]} onClick={this.onClick.bind(this, i)}>
+        <div key={i} className={cx(S.item, { [S.picked] : s.sel === i })}  onClick={this.onClick.bind(this, i)}>
           {v}
         </div> 
       )}
@@ -55,5 +71,5 @@ ToolPanel.propTypes = {
   onPick : PropTypes.func, // 选中一个画刷（图元）的回调 (idx)
 }
 
-export default Radium(ToolPanel);
+export default ToolPanel
 

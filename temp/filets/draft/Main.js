d434dea8347443837b0337b56772dc303d929d51
insanehui <269200808@qq.com>
2016-12-18 17:47:38 +0800

    初步增加输入、输出插口组

--- temp/filets/draft/Main.js
+++ temp/filets/draft/Main.js
@@ -90,32 +90,69 @@ class Main extends Component {
       return <Cls key={i} x={item.x} y={item.y} />
     })
 
+    // 插口组
     let Slots = _.map(s.kits, (item, i ) => {
-      let model = models[item.type] // 取到逻辑model
-      // 暂时先弄输出吧
 
-      let pos = {
-        x : item.x+50,
-        y : item.y,
-        width: 10,
-        height: 10,
-        fill : 'chocolate',
+      let model = models[item.type] // 取到逻辑model
+
+      let gp = {
+        stroke : 'black',
+        strokeWidth : 0.5,
       }
 
-      let Ins = _.map(item.in, (key, type) => {
-        let props = {
+      // 输入插口
+      let Ins = (()=>{
+        let c = 0
 
+        return _.map(model.in, ( type, key) => {
+          let pr = {
+            x : 20 * (c++),
+            y : 90,
+          }
+
+          let rid = `slot_${i}_${key}`
+
+          return <g transform={`translate(${item.x} ${item.y})`} {...gp}>
+            <rect id={rid} width={10} height={10} fill='chocolate' {...pr} />
+            <text {...pr} visibility="hidden">
+              {key}
+              <set attributeName="visibility" from="hidden" to="visible" begin={`${rid}.mouseover`} end={`${rid}.mouseout`} />
+            </text>
+          </g>
+        })
+      })()
+
+      // 输出插口
+      let Outs = (()=>{
+
+        let c = 0
+
+        let outs = model.out
+
+        if ( !_.isObject(outs) ) {
+          outs = { out : outs }
         }
-        return <g>
-          <rect {...pos} />
-          <text {...pos}>{key}</text>
-        </g>
-      })
 
-      return <g>
-        <rect {...pos} />
-        <text {...pos}>{model.out}</text>
-      </g>
+        return _.map(outs, ( type, key) => {
+          let pr = {
+            x : 20 * (c++),
+            y : 0,
+          }
+
+          let rid = `slot_${i}_${key}`
+
+          return <g transform={`translate(${item.x} ${item.y})`} {...gp}>
+            <rect id={rid} width={10} height={10} fill='cornsilk' {...pr} />
+            <text {...pr} visibility="hidden">
+              {key}
+              <set attributeName="visibility" from="hidden" to="visible" begin={`${rid}.mouseover`} end={`${rid}.mouseout`} />
+            </text>
+          </g>
+        })
+
+      })()
+
+      return [...Ins, ...Outs]
 
     })
 

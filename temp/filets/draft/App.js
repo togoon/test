9aa368404c8938604bb7ada6a386e7989d948ef3
insanehui <269200808@qq.com>
2016-12-19 17:27:54 +0800

    初步增加了一些无害的redux代码

--- temp/filets/draft/App.js
+++ temp/filets/draft/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import {HotKeys} from 'react-hotkeys'
-import { createStore } from 'redux'
+import { createStore, combineReducers} from 'redux'
 import { Provider } from 'react-redux'
 
 import {css, border as bd, flex, } from './utils/cssobj.js'
@@ -16,7 +16,25 @@ const S = css({
   },
 })
 
-let store = createStore(() => {})
+// brush的reducer
+function brush(s = null, a) {
+  switch(a.type) {
+    case 'brush_set':
+      return a.val
+    case 'brush_clear':
+      console.log("clear!")
+      return null
+    default:
+      return s
+  }
+}
+
+const app = combineReducers({
+  brush,
+})
+
+
+let store = createStore(app)
 
 class App extends Component {
 
@@ -26,23 +44,30 @@ class App extends Component {
 
   // 选中一个画刷
   onBrush(id) {
+    store.dispatch({type: 'brush_set', val: id})
+
     this.setState({ brush:id })
   }
 
   // 取消画刷
   offBrush() {
     console.log("esc");
+    store.dispatch({type: 'brush_clear'})
+
     this.setState({ brush:null })
   }
 
   render() {
+    const ss = store.getState()
+    console.log("ss", store.getState())
+
     let s = this.state
     return <Provider store={store} >
         <HotKeys className={S.main} handlers={
         {'esc' : this.offBrush.bind(this)}
       } >
         <ToolPanel onPick={this.onBrush.bind(this)} sel={s.brush} />
-        <Main brush={s.brush}/>
+        <Main brush={ss.brush}/>
       </HotKeys>
     </Provider>
   }

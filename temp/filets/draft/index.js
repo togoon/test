e8ce1dcfdf9a9563570c43f0450e765a6ebfefd2
insanehui <269200808@qq.com>
2016-12-20 15:00:19 +0800

    准备将图元的状态信息移到redux，先添加了一些无害的代码，接下来准备进行大调整

--- temp/filets/draft/index.js
+++ temp/filets/draft/index.js
@@ -9,17 +9,64 @@ import {Map as IMap} from 'immutable'
 
 import App_ from './App.js'
 
+// 用immutable来表示整个状态
 let s0 = IMap({
   brush : null,
+
+  kits : IMap({ // 图元数据, immutable
+    m1 : {
+      type : 'mysql',
+      x : 150,
+      y : 100,
+    },
+    m2 : {
+      type : 'mysql',
+      x : 300,
+      y : 100,
+    },
+    s1 : {
+      type : 'storage',
+      x : 100,
+      y : 240,
+    },
+    s2 : {
+      type : 'storage',
+      x : 400,
+      y : 240,
+    },
+  }),
+
+  links : {
+    l1 : {
+      from : 's1',
+      // from_port: // 如果缺省，则为唯一的"out"
+      to : 'm1',
+      to_port: 'volumn',
+    },
+    l2 : {
+      from : 's2',
+      to : 'm2',
+      to_port: 'volumn',
+    },
+  },
+
+  mode : 'normal', // 当前的操作状态，可选值 'grab'
+
+  grabbed_kit: null, // 当前抓住的图元，为其id
+
 })
 
 function reducer(s = s0, a) {
   switch(a.type) {
+
     case 'brush_set':
       return s.set('brush', a.val)
+
     case 'brush_clear':
       console.log("clear!")
       return s.set('brush', null)
+
+
     default:
       return s
   }

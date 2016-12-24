de57daa5c92800e26e96b0b21d5533003fb8c789
insanehui <269200808@qq.com>
2016-12-24 22:32:44 +0800

    写了一部分Property的代码，感觉需要一个editable组件，先放一放

--- temp/filets/draft/Property.js
+++ temp/filets/draft/Property.js
@@ -1,19 +1,45 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux'
 import {w, border} from './utils/cssobj.js'
+import models from './kit_type.js'
+import _ from 'lodash'
 
 class Property extends PureComponent {
 
   render() {
+    const p = this.props
     console.log("property render")
     return <div style={{ ...w(150), ...border}} >
+      <div>
+        {p.model}
+      </div>
+      {_.map(p.inputs, (type, name ) => {
+        return <div key={name}>
+          <div>
+            {name}
+          </div>
+          <div>
+            <input ref={name} style={{...w('80%')}} />
+          </div>
+        </div>
+      })}
     </div>
   }
 }
 
 const sm = (s) => {
+
+  const model = (()=>{
+    if ( s.get('selected_type') !== 'kit' ) {
+      return null
+    } 
+    return s.get('kits').get(s.get('selected')).type
+  })()
+
   return {
-    type: 'xx',
+    model,
+
+    inputs: model ? models[model].in : null,
   }
 }
 

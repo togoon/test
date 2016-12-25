4aa89b916161711510efb7c2b23b8c2f07057649
insanehui <269200808@qq.com>
2016-12-25 20:37:10 +0800

    笔记

--- temp/filets/fetch.js
+++ temp/filets/fetch.js
@@ -2,7 +2,7 @@
 fetch("http://www.example.org/submit.php", {
   method: "POST",
   headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
+    "Content-Type": "application/x-www-form-urlencoded" // 这个header不能少，否则服务端不会按照form的方式来解析参数
   },
   body: "firstName=Nikhil&favColor=blue&password=easytoguess"
 }).then(function(res) {

e47af6d674aaef4041e857ec3e6b97963ff2c40a
insanehui <269200808@qq.com>
2017-03-02 16:47:42 +0800

    注释

--- temp/filets/koa1.js
+++ temp/filets/koa1.js
@@ -1,18 +1,23 @@
 // koa中间件模式初探
 // koa通过中间件的模式来制定http服务器的行为
-// 中间件顺序不同，产生的效果也有一样
+// 其语义非常清晰，见下面示例代码注释：
 
 const Koa = require('koa');
 const app = new Koa();
 
 const middles = [ // 以下都是中间件
 
+  /*
+   *
+   */
   async function (ctx, next) { // 计算响应时间
+    
+    // 1 这是第一个中间件，首先来到这里
 
-    console.log("进入路由")
+    const start = new Date(); // 先做一些事情
 
-    const start = new Date();
-    await next();
+    await next(); // 
+
     const ms = new Date() - start;
 
     ctx.set('time-cost', `${ms}ms`);

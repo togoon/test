9bf813adfbac121c1d72001687758ad6dd3ecb4f
insanehui <269200808@qq.com>
2017-03-20 18:42:30 +0800

    完善注释

--- temp/filets/koa_combine_middleware.js
+++ temp/filets/koa_combine_middleware.js
@@ -9,10 +9,17 @@ const koa_body = koaBody()
 
 const middles = [ 
 
+  /*
+   * 这里实现了一个新的中间件，该中间件koa-body中间件进行包装（对接）
+   */
   async function (ctx, next) {
     ctx.params = ctx.query
     
-    // 对接koa body
+    /*
+     * 注意这里对接中间件的方式：
+     * 将koa-body的next参数传入一个自写的函数
+     * 在该函数里使用其处理的结果，并且接上原本的next
+     */
     await koa_body(ctx, async x=>{
       ctx.params = { ...ctx.params, ...ctx.request.body }
       await next()

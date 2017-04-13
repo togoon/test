a86075dcec48bfb0bf6b858c05ac58ea811eaadc
insanehui <269200808@qq.com>
2017-04-13 22:18:05 +0800

    增加fetch的注释

--- temp/filets/fetch.js
+++ temp/filets/fetch.js
@@ -8,6 +8,10 @@ fetch("http://www.example.org/submit.php", {
 }).then(function(res) {
   if (res.ok) {
     alert("Perfect! Your settings are saved.");
+    // 在这里可以取
+    // res.json(), res.text() 等方法...，但都是promise！
+    // 而且这里似乎有一个坑就是，这些方法只能调用一次？如果调用多次只是逻辑意图上的警告，还是会出错？
+
   } else if (res.status == 401) {
     alert("Oops! You are not authorized.");
   }

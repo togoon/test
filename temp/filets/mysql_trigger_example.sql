9fae975d041d4a9f069552c123a8decb9836850e
insanehui <269200808@qq.com>
2017-05-05 11:27:47 +0800

    mysql_trigger_example.sql

---
+++ temp/filets/mysql_trigger_example.sql
@@ -0,0 +1,32 @@
+-- 触发器示例
+
+--------------------------------------------------------------------
+-- 演示当一个表有update时，自动往另一表插入一条日志信息
+drop trigger if exists upd_state;
+delimiter $$
+-- after/before insert/update/delete 组合，一个表总共有6种情况
+create trigger upd_state after update on mail
+for each row -- 这是固定语句
+begin
+	if new.approve_state != old.approve_state and new.approve_state in ('已启用', '已停用')
+	then
+		insert into state_log values (new.mail_id, now(), new.approve_state);
+	end if;
+end
+$$
+
+delimiter ;
+
+--------------- 演示实现字段default为uuid -----------------------------
+DELIMITER ;;
+CREATE TRIGGER before_insert_tablename
+BEFORE INSERT ON tablename
+FOR EACH ROW
+BEGIN
+  IF new.uuid IS NULL THEN
+    SET new.uuid = uuid();
+  END IF;
+END
+;;
+
+DELIMITER ;

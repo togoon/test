68302420f55f3e1b05034deccc58f314d9a703a9
insanehui <269200808@qq.com>
2017-05-22 14:13:02 +0800

    目录重命名

--- temp/filets/react-motion/3_staggerd_mouse_trace/index.js
+++ temp/filets/react-motion/3_staggerd_mouse_trace/index.js
@@ -1,17 +1,20 @@
 /*
- * 
+ * 这是一个用若干图标来组成一个相互错开的(staggerd)鼠标轨迹的动画效果的例子
+ * 和最基本例子的区别是需要的参数变成了复数：
+ * defaultStyles, styles（变成了一个函数）
  */
 import React from 'react';
 import {StaggeredMotion, spring, presets} from 'react-motion';
 import { render } from 'react-dom'
-import range from 'lodash.range';
+// import range from 'lodash.range';
+import _ from 'lodash'
 
 import './test.css'
 
 export default class Demo extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {x: 250, y: 300};
+    super(props)
+    this.state = {x: 250, y: 300} // 表示当前鼠标状态，整个demo就只有这一个state
   };
 
   componentDidMount() {
@@ -19,12 +22,13 @@ export default class Demo extends React.Component {
     window.addEventListener('touchmove', this.handleTouchMove);
   };
 
-  handleMouseMove = ({pageX: x, pageY: y}) => {
-    this.setState({x, y});
+  handleTouchMove = ({touches}) => { // touch事件先不管
+    this.handleMouseMove(touches[0]);
   };
 
-  handleTouchMove = ({touches}) => {
-    this.handleMouseMove(touches[0]);
+  // 强大！用等号的方法可以解决每次都要手动绑定的问题，太棒了！
+  handleMouseMove = ({pageX: x, pageY: y}) => {
+    this.setState({x, y});
   };
 
   getStyles = (prevStyles) => {
@@ -33,8 +37,10 @@ export default class Demo extends React.Component {
       return i === 0
         ? this.state
         : {
-          x: spring(prevStyles[i - 1].x, presets.gentle),
+          x: spring(prevStyles[i - 1].x, presets.gentle), // presets.gentle是一个动画函数，该设置带一点弹性的效果
           y: spring(prevStyles[i - 1].y, presets.gentle),
+          // x: spring(prevStyles[i - 1].x),
+          // y: spring(prevStyles[i - 1].y),
         };
     });
     return endValue;
@@ -43,7 +49,7 @@ export default class Demo extends React.Component {
   render() {
     return (
       <StaggeredMotion
-        defaultStyles={range(6).map(() => ({x: 0, y: 0}))}
+        defaultStyles={_.range(6).map(() => ({x: 0, y: 0}))}
         styles={this.getStyles}>
         {balls =>
           <div className="demo1">
@@ -52,8 +58,7 @@ export default class Demo extends React.Component {
                 key={i}
                 className={`demo1-ball ball-${i}`}
                 style={{
-                  WebkitTransform: `translate3d(${x - 25}px, ${y - 25}px, 0)`,
-                  transform: `translate3d(${x - 25}px, ${y - 25}px, 0)`,
+                  transform: `translate3d(${x - 25}px, ${y - 25}px, 0)`, // 这里25的修正值是因为ball的直径为50
                   zIndex: balls.length - i,
                 }} />
             )}

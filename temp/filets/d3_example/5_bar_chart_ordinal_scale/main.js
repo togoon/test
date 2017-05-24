1b9fd802424f0bfe1069bf4805a80129cb6fe8b7
insanehui <269200808@qq.com>
2017-05-24 16:37:13 +0800

    修复版本的兼容性问题，增加注释

--- temp/filets/d3_example/5_bar_chart_ordinal_scale/main.js
+++ temp/filets/d3_example/5_bar_chart_ordinal_scale/main.js
@@ -1,10 +1,22 @@
+/*
+ * 介绍ordinal scale中band scale的使用
+ * 最简单的ordinal scale本例中没有涵盖，而是直接使用了进阶的band scale
+ * band scale 就是将离散的点映射到连续域中的一段一段（简直就是为柱状图案量身定做的）
+ * 事实上composer里输入输出的点的分布，就可以使用上band scale
+ * 之前我自己实现的算法，至今还不能完全居中
+ */
+
 var width = 960,
     height = 500;
 
-var x = d3.scaleOrdinal()
-    .rangeRoundBands([0, width], .1);
+/*
+ * x轴用的是band scale
+ * range（值域）与linear scale一致
+ * round(true) 好像是用来设置两边留空？
+ */
+var x = d3.scaleBand().range([0, width]).round(true).padding(0.1)
 
-var y = d3.scale.linear()
+var y = d3.scaleLinear()
     .range([height, 0]);
 
 var chart = d3.select(".chart")
@@ -12,6 +24,7 @@ var chart = d3.select(".chart")
     .attr("height", height);
 
 d3.tsv("data.tsv", type, function(error, data) {
+
   x.domain(data.map(function(d) { return d.name; }));
   y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
@@ -23,10 +36,10 @@ d3.tsv("data.tsv", type, function(error, data) {
   bar.append("rect")
       .attr("y", function(d) { return y(d.value); })
       .attr("height", function(d) { return height - y(d.value); })
-      .attr("width", x.rangeBand());
+      .attr("width", x.bandwidth());
 
   bar.append("text")
-      .attr("x", x.rangeBand() / 2)
+      .attr("x", x.bandwidth() / 2)
       .attr("y", function(d) { return y(d.value) + 3; })
       .attr("dy", ".75em")
       .text(function(d) { return d.value; });

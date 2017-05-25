c7518374b759c92dc97a52ad54060fc37dcbd467
insanehui <269200808@qq.com>
2017-05-25 15:39:08 +0800

    完成dendrogram的注释

--- temp/filets/d3_example/dendrogram/main.js
+++ temp/filets/d3_example/dendrogram/main.js
@@ -13,6 +13,8 @@ var tree = d3.cluster()
   /*
    * 这个size好像是用来指定坐标尺寸的。
    * 因为最终cluster对象会给一个层次数据结构分配坐标，因为需要知道尺寸信息
+   *
+   * 注意：这里height放在了前面，cluster默认是金字塔的朝向，而本例是向右扩散的方向，因此坐标要调换一下
    */
   .size([height, width - 160]);
 
@@ -23,7 +25,8 @@ var tree = d3.cluster()
 var stratify = d3.stratify()
   .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); }); // 需要指定一个获取父节点id的方法
 
-d3.csv("flare.csv", function(error, data) {
+d3.csv("simple.csv", function(error, data) {
+// d3.csv("flare.csv", function(error, data) {
   if (error) throw error;
 
   // 得到解析后的层级关系对象
@@ -42,21 +45,28 @@ d3.csv("flare.csv", function(error, data) {
    * 接下来就可以拿root里的数据来渲染出酷炫的树状图了
    */
 
-  console.log("descendants", root.descendants())
+  /*
+   * root的descendants() 取得了所有的节点，包括根节点
+   * 似乎是按照金字塔顺序来排的序，还是说使用者不应该依赖其顺序？
+   */
+  const descendants = root.descendants()
+  console.log("descendants", descendants)
 
+  // 绘制所有边，除根节点之外，任何一个节点前边，都有一条边
   var link = g.selectAll(".link")
-      .data(root.descendants().slice(1))
+      .data(descendants.slice(1))
     .enter().append("path")
       .attr("class", "link")
       .attr("d", function(d) {
         return "M" + d.y + "," + d.x
-            + "C" + (d.parent.y + 100) + "," + d.x
+            + "C" + (d.parent.y + 100) + "," + d.x // 贝塞尔曲线的控制杆的长度写死为100（并不是像processOn里的动态计算），事实上也不影响美观
             + " " + (d.parent.y + 100) + "," + d.parent.x
             + " " + d.parent.y + "," + d.parent.x;
       });
 
+  // 绘制所有节点
   var node = g.selectAll(".node")
-      .data(root.descendants())
+      .data(descendants)
     .enter().append("g")
       .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
       .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })

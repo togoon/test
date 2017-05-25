4744380e5c87147bbb3984b1a6415507fae44fd1
insanehui <269200808@qq.com>
2017-05-25 17:19:30 +0800

    注释hierarchy

--- temp/filets/d3_example/circle_packing/main.js
+++ temp/filets/d3_example/circle_packing/main.js
@@ -16,14 +16,24 @@ var pack = d3.pack()
 d3.json("flare.json", function(error, root) {
   if (error) throw error;
 
-  root = d3.hierarchy(root)
-
   /*
    * 使用d3.pack必须要调用sum方法
    * node.sum需要传入一个获取节点"权重"的函数，也许d3需要这个值来决定每个pack的大小
    */
+  root = d3.hierarchy(root)
       .sum(function(d) { return d.size; })
       .sort(function(a, b) { return b.value - a.value; });
+
+  /*
+   * hierarchy之后的root经过一定的结构调整，每个节点获得了以下更加强大的属性:
+   * data: 为了保持现场，原来的数据被原封不动地移到了这里
+   * depth: 深度，最顶级为0
+   * height: 它到最远的叶子的长度
+   * parent: 父节点，根节点为null
+   * children: 子节点
+   * value: 其sum值。见node.sum
+   */
+  console.log("root", root)
 
   var node = g.selectAll(".node")
     /*

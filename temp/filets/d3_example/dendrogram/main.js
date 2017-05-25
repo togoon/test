317577363aa57da38899e6dd469ee1d64a5c3149
insanehui <269200808@qq.com>
2017-05-25 14:47:19 +0800

    补充了大量的注释

--- temp/filets/d3_example/dendrogram/main.js
+++ temp/filets/d3_example/dendrogram/main.js
@@ -1,6 +1,8 @@
 /*
  * 一个树状图的demo
  * 非常经典！也就是常见的思维导图采用的ui形式
+ *
+ * 演示cluster的使用
  */
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
@@ -8,18 +10,37 @@ var svg = d3.select("svg"),
     g = svg.append("g").attr("transform", "translate(40,0)");
 
 var tree = d3.cluster()
-    .size([height, width - 160]);
+  /*
+   * 这个size好像是用来指定坐标尺寸的。
+   * 因为最终cluster对象会给一个层次数据结构分配坐标，因为需要知道尺寸信息
+   */
+  .size([height, width - 160]);
 
+/*
+ * stratify是一个非常重要的函数: 
+ * 将一个二维关系表格拼装成树状结构
+ */
 var stratify = d3.stratify()
-    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+  .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); }); // 需要指定一个获取父节点id的方法
 
 d3.csv("flare.csv", function(error, data) {
   if (error) throw error;
 
+  // 得到解析后的层级关系对象
   var root = stratify(data)
-      .sort(function(a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); });
+    /*
+     * 调用sort方法，可以令数据在保持层次结构的同时，处于同一层级的节点能按照一定的顺序排序，详见文档
+     */
+    .sort(function(a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); });
 
+  /*
+   * d3 cluster的精髓，给root里的每个节点注入布局时需要用到的坐标
+   */
   tree(root);
+
+  /*
+   * 接下来就可以拿root里的数据来渲染出酷炫的树状图了
+   */
 
   var link = g.selectAll(".link")
       .data(root.descendants().slice(1))

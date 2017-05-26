cf45fb0831bab33a7ae522b15c96771ab38d84ce
insanehui <269200808@qq.com>
2017-05-26 17:36:49 +0800

    .

--- temp/filets/d3_example/11_tween_smooth_scrolling/main.js
+++ temp/filets/d3_example/11_tween_smooth_scrolling/main.js
@@ -1,7 +1,11 @@
-
+/*
+ * 这里的tween似乎跟react-motion里的spring有点像
+ * 但似乎可以自定义更多的细节
+ */
 d3.transition()
     .delay(1500)
     .duration(7500)
+    // 这里 'scroll' 的名字可以任意取，它主要是给删除的时候用的
     .tween("scroll", scrollTween(document.body.getBoundingClientRect().height - window.innerHeight));
 
 function scrollTween(offset) {

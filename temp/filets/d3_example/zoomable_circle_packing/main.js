7d171acc5e6f021afa8e0c66b00e1181f04565ff
insanehui <269200808@qq.com>
2017-05-26 10:36:18 +0800

    增加注释

--- temp/filets/d3_example/zoomable_circle_packing/main.js
+++ temp/filets/d3_example/zoomable_circle_packing/main.js
@@ -1,13 +1,23 @@
+/*
+ * zoomable的 circle packing
+ * > 演示d3-scale在颜色插值上的应用（即通过颜色的变化来直观表示一个值的变化）
+ * > 演示如何在d3上响应事件
+ */
 var svg = d3.select("svg"),
     margin = 20,
     diameter = +svg.attr("width"),
     g = svg.append("g").attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
+/*
+ * 指定一个颜色的插值scale. 通过颜色的渐变来反映层次的深度
+ * 前面基础的circle packing demo里没有使用这个技术，是用了一个简便的trick：通过设置一个颜色的透明度叠加而成
+ */ 
 var color = d3.scaleLinear()
     .domain([-1, 5])
     .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
     .interpolate(d3.interpolateHcl);
 
+// 定义一个circle packing
 var pack = d3.pack()
     .size([diameter - margin, diameter - margin])
     .padding(2);
@@ -15,20 +25,26 @@ var pack = d3.pack()
 d3.json("flare.json", function(error, root) {
   if (error) throw error;
 
-  root = d3.hierarchy(root)
+  root = d3.hierarchy(root) // hierarchy化，与上例相同
       .sum(function(d) { return d.size; })
       .sort(function(a, b) { return b.value - a.value; });
 
-  var focus = root,
-      nodes = pack(root).descendants(),
-      view;
+  var focus = root, // 指当前选中哪个节点，初始是root
+    nodes = pack(root).descendants(), // 所有节点平铺
+    view;
 
   var circle = g.selectAll("circle")
     .data(nodes)
     .enter().append("circle")
-      .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
-      .style("fill", function(d) { return d.children ? color(d.depth) : null; })
-      .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
+    .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+    /*
+     * 使用颜色坐标！酷！
+     */
+    .style("fill", function(d) { return d.children ? color(d.depth) : null; }) 
+    /*
+     * 响应点击事件
+     */
+    .on("click", function(d) { if (focus !== d) zoom(d), d3.event.stopPropagation(); });
 
   var text = g.selectAll("text")
     .data(nodes)
@@ -44,13 +60,30 @@ d3.json("flare.json", function(error, root) {
       .style("background", color(-1))
       .on("click", function() { zoom(root); });
 
+  /*
+   * zoom算法是核心关键点
+   */
+
+
+  /*
+   * 这个zoomTo只是一些数据的初始化，并没有调用zoom函数
+   */
   zoomTo([root.x, root.y, root.r * 2 + margin]);
 
+
+  /*
+   * 传入一个节点（数据节点），zoom到以其为中心的视角
+   */
   function zoom(d) {
-    var focus0 = focus; focus = d;
+    console.log("zoom!!")
+
+    var focus0 = focus; focus = d; // 记录原来的focus，更新当前的focus
 
     var transition = d3.transition()
-        .duration(d3.event.altKey ? 7500 : 750)
+    /*
+     * 亮点！还可以根据altkey来放慢速度！！
+     */
+      .duration(d3.event.altKey ? 7500 : 750)
         .tween("zoom", function(d) {
           var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + margin]);
           return function(t) { zoomTo(i(t)); };

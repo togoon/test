310a732a32e784dfd0d1b12ea2f46508575ba320
insanehui <269200808@qq.com>
2017-05-27 11:12:02 +0800

    再次加深了对tween的理解

--- temp/filets/d3_example/11_tween_smooth_scrolling/main.js
+++ temp/filets/d3_example/11_tween_smooth_scrolling/main.js
@@ -4,7 +4,12 @@
 d3.transition()
     .delay(1500)
     .duration(7500)
-    // 这里 'scroll' 的名字可以任意取，它主要是给删除的时候用的
+    /*
+     * 这里 'scroll' 的名字可以任意取，它主要是给删除的时候用的
+     * 第二个参数是一个函数，其原型为： (d,...) => t => {...}
+     * 最终返回一个时间函数，其里面可以根据传入的时间插值来干任何事情，主要就是指定到底要如何体现动画
+     * 思想很接近react motion
+     */
     .tween("scroll", scrollTween(document.body.getBoundingClientRect().height - window.innerHeight));
 
 function scrollTween(offset) {

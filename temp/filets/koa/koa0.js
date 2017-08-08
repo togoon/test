aa8be06a18793cc05c13b312f69c65b811f1bf72
insanehui <269200808@qq.com>
2017-08-08 17:40:24 +0800

    去掉分号

--- temp/filets/koa/koa0.js
+++ temp/filets/koa/koa0.js
@@ -1,10 +1,10 @@
 // koa的最简单示例
 
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const app = new Koa()
 
 app.use(ctx => {
-  ctx.body = 'hello koa'; // 一个http服务器，任何请求返回haha
-});
+  ctx.body = 'hello koa' // 一个http服务器，任何请求返回haha
+})
 
-app.listen(3000);
+app.listen(3001)

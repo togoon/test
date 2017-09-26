e337c2cd896cc3d087f84e7f70721d3607348c26
insanehui <269200808@qq.com>
2017-09-26 10:38:24 +0800

    演示一些常见事件管理的操作

--- temp/filets/rxjs/02_purity.js
+++ temp/filets/rxjs/02_purity.js
@@ -1,7 +1,7 @@
 // React
 /*
  * rx更便于把事件管理纯化
- * 示例：点击时统计点击的次数
+ * 增加一些简单的事件管理，详见下文注释
  */
 import React, { PureComponent } from 'react'
 import { render } from 'react-dom'
@@ -9,15 +9,42 @@ import Rx from 'rxjs/Rx'
 
 class App extends PureComponent {
 
+  /*
+   * 统计点击次数
+   */
   button = el=>{
     Rx.Observable.fromEvent(el, 'click')
       .scan(count => count + 1, 0) // 有点类似array的reduce，用来统计点击次数
       .subscribe(count => console.log(`Clicked ${count} times`))
   }
 
+  /*
+   * 点击限流
+   */
+  throttle = el=>{
+    Rx.Observable.fromEvent(el, 'click')
+      .throttleTime(1000)
+      .scan(count => count + 1, 0)
+      .subscribe(count => console.log(`限流点击 ${count} 次`));
+  }
+
+  /*
+   * map的一个小示例: 每点击一次累加一下clientX
+   * 没有实际的意义，仅为了演示map的用法
+   */
+  map = el=>{
+    Rx.Observable.fromEvent(el, 'click')
+      .throttleTime(1000)
+      .map(event => event.clientX) 
+      .scan((count, clientX) => count + clientX, 0)
+      .subscribe(count => console.log(count));
+  }
+
   render() {
     return <div>
       <button ref={this.button}>点击一下</button>
+      <button ref={this.throttle}>限流</button>
+      <button ref={this.map}>map</button>
     </div>
   }
 }

e0969e517e94a8f74da62ce528c6000657bce71c
insanehui <269200808@qq.com>
2017-09-26 15:21:36 +0800

    注释behavior subject和普通subject的区别

--- temp/filets/rxjs/09_behavior_subject.js
+++ temp/filets/rxjs/09_behavior_subject.js
@@ -3,6 +3,8 @@ import Rx from 'rxjs/Rx'
 
 /*
  * behavior subject: 保存随着时间变化的一个值
+ * 其与普通subject的区别是：behavior subject在新订阅时都会收到当前的最新值
+ * 而普通subject则没有该行为，需要等到下一个值触发才会收到
  */
 var subject = new Rx.BehaviorSubject(0); // 0 is the initial value
 

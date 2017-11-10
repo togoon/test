93e37294398836681da21147cdff3134c8cdbd62
insanehui <269200808@qq.com>
2017-11-10 17:10:14 +0800

    进一步注释mobx

--- temp/filets/mobx/01_first.js
+++ temp/filets/mobx/01_first.js
@@ -7,10 +7,16 @@ import React, { Component } from 'react'
 import {observable, action} from 'mobx';
 import {observer} from 'mobx-react';
 
-var appState = observable({
+/*
+ * 建一个observable的数据源
+ */
+var appState = observable({ 
     timer: 0
 })
 
+/*
+ * 对observable的操作，用action包起来
+ */ 
 appState.resetTimer = action(function reset() {
     appState.timer = 0;
 })
@@ -19,15 +25,18 @@ setInterval(action(function tick() {
     appState.timer += 1;
 }), 1000)
 
+/*
+ * 用observer来修饰组件
+ */
 @observer
 class TimerView extends Component { // 不能使用pure component 
   render() {
-    return (<button onClick={this.onReset.bind(this)}>
+    return (<button onClick={this.reset}>
       Seconds passed: {this.props.appState.timer}
     </button>);
   }
 
-  onReset () {
+  reset = ()=>{
     this.props.appState.resetTimer();
   }
 };

e7865ec3e94a1c15fb44f5864f614759dadd95db
insanehui <269200808@qq.com>
2017-11-10 17:55:18 +0800

    完善注释

--- temp/filets/decorator.js
+++ temp/filets/decorator.js
@@ -11,7 +11,10 @@ const log = (target, name, descriptor)=>{
    * 其实还是很晦涩，暂时不去深究其用意
    */
 
-  // 在本例里，descriptor.value才是我们要修饰的方法，将其取出
+  /*
+   * 如果该装饰器是用来修饰类，则target就是其修饰的类
+   * 如果装饰器修饰的是类或者对象的方法，则要取descriptor.value
+   */
   const {value} = descriptor
 
   descriptor.value = (...para)=>{ // 将其重新包装。这里才是装饰器的核心逻辑

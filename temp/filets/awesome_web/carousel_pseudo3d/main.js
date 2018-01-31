0ac0eac5d31442686795cc3d3a38a7a82604a1ab
insanehui <269200808@qq.com>
2018-01-31 12:10:43 +0800

    增加注释

--- temp/filets/awesome_web/carousel_pseudo3d/main.js
+++ temp/filets/awesome_web/carousel_pseudo3d/main.js
@@ -6,11 +6,13 @@
     $main.children().off('click')
 
     $main.children(':lt(2)').click(function(){
+      // 最后一个移到最前面
       $main.children(':last').prependTo($main)
       setup()
     })
 
     $main.children(':eq(3),:eq(4)').click(function(){
+      // 第一个移到最后面
       $main.children(':first').appendTo($main)
       setup()
     })

bbcd1805bf6dc5a7900892fde7da825277e7b758
insanehui <269200808@qq.com>
2018-03-30 14:21:38 +0800

    注释一下

--- temp/filets/webpack_demo/01_basic/webpack.config.js
+++ temp/filets/webpack_demo/01_basic/webpack.config.js
@@ -1,5 +1,9 @@
 /*
- * webpack的配置，注意最好要使用不需要babel转换的原生node.js语法
+ * 最简单的webpack的配置 [注意最好要使用不需要babel转换的原生node.js语法]
+ *
+ * 最简单的场景只需要配一个entry
+ * 然后使用缺省的配置文件名：webpack.config.js，这时只需要在当前目录下执行：webpack
+ * 则会生成 ./dist/[name].js
  */
 const path = require('path')
 const d = path.resolve.bind(null, __dirname)

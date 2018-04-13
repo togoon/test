6fd02de2f6b8b9c983c3c6d8e0fa7d9b19e8286b
insanehui <269200808@qq.com>
2018-04-13 15:09:56 +0800

    更新output的注释

--- temp/filets/webpack_demo/04_output/webpack.config.js
+++ temp/filets/webpack_demo/04_output/webpack.config.js
@@ -9,7 +9,9 @@ module.exports = {
   entry : d('../src01/main.js'),
   /*
    * 如果整个output都缺省，filename取为[name].js
-   * path则为d(process.cwd(),'dist')，即'./dist'
+   *  path则为d(process.cwd(),'dist')，即'./dist'
+   * 如果指定了filename（filename里包含的目录会忽略），缺省path
+   *  path则为当前目录
    */
   output : { // <url:https://webpack.js.org/configuration/output/>
     filename : '[name].js', 

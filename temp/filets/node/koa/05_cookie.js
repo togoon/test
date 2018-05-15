082d547e70195386acf0a64599666cdb8d321a9d
insanehui <269200808@qq.com>
2018-05-15 23:11:11 +0800

    更新cookie的使用

--- temp/filets/node/koa/05_cookie.js
+++ temp/filets/node/koa/05_cookie.js
@@ -3,23 +3,25 @@
  */
 const Koa = require('koa')
 const app = new Koa()
+process.title = 'httpSvr'
 
 app.use( async ( ctx ) => {
   if ( ctx.url === '/index' ) {
     ctx.cookies.set(
       'cid', 
-      'hello world',
+      'haha',
       {
-        domain: 'localhost',  // 写cookie所在的域名
-        path: '/index',       // 写cookie所在的路径
-        maxAge: 10 * 60 * 1000, // cookie有效时长
-        expires: new Date('2017-02-15'),  // cookie失效时间
-        httpOnly: false,  // 是否只用于http请求中获取
-        overwrite: false  // 是否允许重写
+        // domain: 'localhost', 
+        // path: '/index',       
+        maxAge: 2 * 60 * 1000, // 两分钟的cookie
+        // expires: new Date('2017-02-15'),
+        // httpOnly: false, 
+        // overwrite: false  // 暂时不知道用处
       }
     )
     ctx.body = 'cookie is ok'
   } else {
+    console.log(`======== ${ctx.path} ======\ncookies:`, ctx.header.cookie)
     ctx.body = 'hello world' 
   }
 })

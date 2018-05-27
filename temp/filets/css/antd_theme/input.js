fe3f4f3dc231e59c7b369a9c5a85eebfae4f7298
insanehui <269200808@qq.com>
2018-05-27 17:30:26 +0800

    初步调了样式

--- temp/filets/css/antd_theme/input.js
+++ temp/filets/css/antd_theme/input.js
@@ -3,18 +3,58 @@ import {render} from 'react-dom'
 import _ from 'lodash'
 import injectSheet from 'react-jss'
 
+import './iconfont.css'
+
+const $ = (...p)=>_.merge({}, comm, ...p)
+
+const lightGray = 'rgba(0,0,0,.25)'
+
+const iconify = C => ({icon, ...props}) => {
+  const padding = 30
+
+  return <span style={{display : 'inline-block',position : 'relative'}} >
+    <C {...$({style:{paddingLeft:padding - 3}}, props)} />
+    <div style={{
+      position : 'absolute',
+      left : 0,
+      top : 0,
+      width : padding,
+      height : '100%',
+      display : 'flex',
+      justifyContent : 'center', 
+      alignItems : 'center',
+      color : 'lightGray',
+    }}>
+      <i class={`iconfont icon-${icon}`} />
+    </div>
+  </span>
+}
+
+const Input = iconify('input')
+
 const comm = {
   padding: "4px 11px",
   color: "rgba(0, 0, 0, 0.65)",
   borderRadius: "4px",
   borderStyle : 'solid',
   borderWidth : 1,
+
+  width : 193,
+  height : 32,
+  boxSizing : 'border-box',
+
   transition: ".3s",
   '&:focus' : {
     outline : 'none',
   },
+  '&::-webkit-input-placeholder' : {
+    color : lightGray,
+  },
 }
-const $ = (...p)=>_.merge({}, comm, ...p)
+
+function Gap() {
+  return <div style={{display : 'inline-block',width:20}} />
+}
 
 @injectSheet({
   '@global' : {
@@ -58,13 +98,10 @@ class Test extends PureComponent {
   render() {
     const {classes:{cinput, cinput_error, cmsg}} = this.props
     return <div>
-      <h1>
-        <input className={cinput} />
-      </h1>
-      <h1>
-        <input className={cinput_error} />
-        <div className={cmsg}>请输入正确的信息</div>
-      </h1>
+      <Input icon='people' className={cinput} placeholder='Username' />
+      <Gap />
+      <Input icon='newshot' className={cinput_error} placeholder='Password' />
+      <div className={cmsg}>请输入正确的信息</div>
     </div>
   }
 }

4b401435538166997864b53d058260cf4e6abd63
insanehui <269200808@qq.com>
2018-05-31 20:38:21 +0800

    .

--- temp/filets/css/float/size_grow_in_a_flex_item.js
+++ temp/filets/css/float/size_grow_in_a_flex_item.js
@@ -1,7 +1,6 @@
 /*
  * 如果在一个flex item里，情况有点微妙
  * > 不出现parent collapse现象
- * > float元素和散装的inline会参与试图撑开宽度
  */
 import React, { PureComponent} from 'react'
 import {render} from 'react-dom'
@@ -11,25 +10,26 @@ class Test extends PureComponent {
     return <div style={{
       width : 300,
       display : 'flex',
-      border : '1px solid red',
+      border : '2px solid red',
     }} >
       <div style={{
-        border : '1px solid gray',
+        border : '2px solid gray',
         boxSizing : 'border-box',
         flex : 1,
       }} >
         <div style={{
           width : 80,
           height : 80,
-          border : '1px solid green',
+          border : '2px solid green',
           float : 'left',
+          margin : 10,
         }} >
           A
         </div>
         <div style={{
           width : 80,
           height : 80,
-          border : '1px solid lightblue',
+          border : '2px solid lightblue',
           float : 'right',
         }} >
           B

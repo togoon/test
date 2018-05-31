96304bfba98ca44d860be7cb91b9c31a9016a014
insanehui <269200808@qq.com>
2018-05-31 11:16:48 +0800

    注释

--- temp/filets/css/padding/too_much.js
+++ temp/filets/css/padding/too_much.js
@@ -1,7 +1,7 @@
 /*
  * 对于content-box的box-sizing，padding是不参与元素大小的约束
  * 而对于border-box，padding如果过大将会把元素撑开（无视原本尺寸约束）
- * 这时content的尺寸是0 * 0，里面的内容默认的情况下会以wrap的形式来显示
+ * 这时content的尺寸是0 * 0，会根据wrap的机制来显示元素
  */
 import React, { PureComponent} from 'react'
 import {render} from 'react-dom'
@@ -12,7 +12,7 @@ class Test2 extends PureComponent {
       <div style={{
         width : 100,
         height : 100,
-        padding : 80,
+        padding : 80, // padding很大，将无视原来的width, height设置
         border : '1px solid gray',
         boxSizing : 'border-box',
       }} >

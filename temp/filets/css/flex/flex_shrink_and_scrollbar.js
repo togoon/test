fba0c5af7d8a65af4d51761a3ab930b9e7cabc50
insanehui <269200808@qq.com>
2018-06-05 19:42:55 +0800

    注释flex-shrink的问题

--- temp/filets/css/flex/flex_shrink_and_scrollbar.js
+++ temp/filets/css/flex/flex_shrink_and_scrollbar.js
@@ -1,5 +1,7 @@
 /*
  * flex容器溢出的时候，什么时候会flex-shrink，什么时候会滚动条？
+ * flex-shrink的缺省值是1，表面上似乎意味着其可以被无限压缩，但实际上不会压缩到挤压子元素的程度，这种情况下就会溢出或者出现滚动条
+ * 但如果该元素本身又是一个flex容器，则意味着它具备了挤压子元素的能力。而其子元素又能否被挤压，则递归地应用这条规则来判定
  */
 import React, { PureComponent } from 'react'
 import {render,} from 'react-dom'
@@ -16,7 +18,7 @@ class Test extends PureComponent {
       <div style={{
         /*
          * 这里是否指定display:flex很关键。
-         * 如果注释掉flex，则容器不会shrink到压缩子元素。这时就会容器出现滚动条
+         * 如果注释掉flex，则容器不会shrink到压缩子元素。这时容器就会出现滚动条
          */
         // display : 'flex',
         flexDirection : 'inherit',

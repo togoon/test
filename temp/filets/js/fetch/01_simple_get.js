76fb8c74d06ff25d4567acea5d75fc992c64508b
insanehui <269200808@qq.com>
2018-06-11 16:09:17 +0800

    目录结构小调整

---
+++ temp/filets/js/fetch/01_simple_get.js
@@ -0,0 +1,11 @@
+const xhr = new XMLHttpRequest()
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1')
+xhr.onreadystatechange = ()=>{
+  /*
+   * 0 - 4分别对应：UNSENT, OPENED, HEADERS_RECEIVED, LOADING, DONE
+   */
+  if ( xhr.readyState === 4) {
+    console.log(xhr.responseText)
+  } 
+}
+xhr.send()

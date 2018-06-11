4d374e68ef23f1bfd17137bc3727888b276389dc
insanehui <269200808@qq.com>
2018-06-11 16:24:02 +0800

    增加了fetch的simple get

--- temp/filets/js/ajax/01_simple_get.js
+++ temp/filets/js/ajax/01_simple_get.js
@@ -5,7 +5,7 @@ xhr.onreadystatechange = ()=>{
    * 0 - 4分别对应：UNSENT, OPENED, HEADERS_RECEIVED, LOADING, DONE
    */
   if ( xhr.readyState === 4) {
-    console.log(xhr.responseText)
+    console.log(xhr.status, xhr.responseText)
   } 
 }
 xhr.send()
--- temp/filets/js/fetch/01_simple_get.js
+++ temp/filets/js/fetch/01_simple_get.js
@@ -1,11 +1,6 @@
-const xhr = new XMLHttpRequest()
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1')
-xhr.onreadystatechange = ()=>{
-  /*
-   * 0 - 4分别对应：UNSENT, OPENED, HEADERS_RECEIVED, LOADING, DONE
-   */
-  if ( xhr.readyState === 4) {
-    console.log(xhr.responseText)
-  } 
-}
-xhr.send()
+(async ()=>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+  console.log(res.status)
+  const data = await res.json()
+  console.log(data)
+})()

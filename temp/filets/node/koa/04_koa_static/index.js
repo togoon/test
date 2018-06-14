3b54ee6fc47f75b697d3fd40541127404ea15411
insanehui <269200808@qq.com>
2018-06-14 18:48:12 +0800

    演示一下maxage

--- temp/filets/node/koa/04_koa_static/index.js
+++ temp/filets/node/koa/04_koa_static/index.js
@@ -3,13 +3,13 @@ const path = require('path')
 const serve = require('koa-static')
 const app = new Koa()
 
-process.title = 'koa'
+process.title = 'koaStatic'
 
 // 静态资源目录对于相对入口文件index.js的路径
 const staticPath = './static'
 
 app.use(serve(
-  path.join( __dirname,  staticPath)
+  path.join( __dirname,  staticPath), {maxage:1000*60}
 ))
 
 app.listen(51312, () => {

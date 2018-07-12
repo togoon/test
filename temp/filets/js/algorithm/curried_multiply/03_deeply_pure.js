b7c8b15679f6475789ee6bc470dbdfafbe02c915
insanehui <269200808@qq.com>
2018-07-12 17:10:47 +0800

    一个非常精髓的柯里化实现

--- temp/filets/js/algorithm/curried_multiply/03_deeply_pure.js
+++ temp/filets/js/algorithm/curried_multiply/03_deeply_pure.js
@@ -1,23 +1,17 @@
 /*
  * 一个完全pure的柯里化的mul
- * 重点：
- * > mul对象调用一次之后，会返回其自身的一个副本，如何实现？
  */
-
 function mul(x) {
-  function closure(x) {
-    let cache = x
-    function op(y) {
-      cache *= y
-      return closure(cache)
-    }
-    op.toString = ()=>cache
-    return op
+  let cache = x
+  function op(y) {
+    return mul(y*x)
   }
+  op.toString = ()=>cache
 
-  return closure(x)
+  return op
 }
 
 const m2 = mul(2)
 console.log(m2(3))
-console.log(m2(4)) // 这里还是不能重复使用
+console.log(m2(4)) 
+console.log(m2(5)(6))

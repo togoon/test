d5c62a9f11824c44ddd4f7ed44aee8e5b0ad2cc5
insanehui <269200808@qq.com>
2018-10-09 16:10:32 +0800

    初步增加echarts的初探

---
+++ temp/filets/js_lib/echarts/01.js
@@ -0,0 +1,43 @@
+/*
+ * echarts的初探示例
+ */
+import React from 'react'
+import { render, } from 'react-dom'
+import echarts from 'echarts'
+
+class Test extends React.PureComponent {
+  componentDidMount(){
+    const myChart = echarts.init(this.refs.chart);
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    });
+  }
+
+  render() {
+    return <div>
+      echarts会适应dom元素的尺寸进行画图，如果dom元素尺寸发生改变，图表默认不会自动调整
+      <div style={{
+        width : 500,
+        height : 500,
+        border : '1px solid gray',
+      }} ref='chart' />
+    </div>
+  }
+}
+
+render(<Test />, document.getElementById('root'))
+
+

54f07ba6315354725ade7b82568027caa4ea41d2
insanehui <269200808@qq.com>
2018-12-21 01:03:13 +0800

    增加用例

--- temp/filets/js_lib/mathjs/fractions.js
+++ temp/filets/js_lib/mathjs/fractions.js
@@ -3,3 +3,7 @@ import {fraction, add} from 'mathjs'
 const a = add(fraction(1,3), fraction(2, 5))
 console.log('a', a) // fraction原始对象
 console.log('a', a.toFraction()) // 以分数的形式打印
+
+const b = fraction(4)
+const c = add(b.div(4), 0.2)
+console.log('c', c)

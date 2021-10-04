3c06a68cee100e7df17fa69bf4cdebd7f831ef77
insanehui <269200808@qq.com>
2021-10-04 11:47:59 +0800

    .

--- temp/filets/mini_program/app.json.js
+++ temp/filets/mini_program/app.json.js
@@ -5,9 +5,14 @@
   ],
   "window":{
     "backgroundTextStyle":"light",
+
+    /*
+     * 以下两个设置必须要有，否则界面头顶上是黑的
+     */
     "navigationBarBackgroundColor": "#fff",
+    "navigationBarTextStyle":"black",
+
     "navigationBarTitleText": "xxx", /* 标题 */
-    "navigationBarTextStyle":"black"
   },
   "style": "v2",
   "sitemapLocation": "sitemap.json"

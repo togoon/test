8080ab32279c0eaad21620e0b55878e6d1939c88
insanehui <269200808@qq.com>
2021-10-17 10:43:39 +0800

    .

--- temp/filets/tongdaxin/macd.hs
+++ temp/filets/tongdaxin/macd.hs
@@ -3,6 +3,12 @@
  - long: 26
  - mid: 9
  -}
+
+{- 短线平均 - 长线平均，只不过是指数移动平均 -}
 DIF:EMA(CLOSE,SHORT)-EMA(CLOSE,LONG);
+
+{- dea对dif再求一个中线平均 -}
 DEA:EMA(DIF,MID);
+
+{- 得到dif与dea的差，并且以柱状线显示 -}
 MACD:(DIF-DEA)*2,COLORSTICK;
